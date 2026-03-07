bd4ba02579ec8474572a703f70f26dda6254c278
渡部駿希 <syunk9521@gmail.com>
2026-03-07 21:03:28 +0900

    usecaseの実装

--- app/domain/body_info/enums/activity_level.py
+++ app/domain/body_info/enums/activity_level.py
@@ -1,11 +1,20 @@
 from enum import Enum
 
 
-class ActivityLStatus(str, Enum):
+class ActivityStatus(str, Enum):
+    """
+    MEMO: DB保存時の値
+    orm.activity = status.value
+    """
     OFFICE = "OFFICE"
 
     @property
     def label(self):
         return {
+            """
+            MEMO: 画面表示用
+            status = ActivityLStatus.OFFICE
+            responses = { "activity": status.label }
+            """
             "OFFICE": "オフィスワーク",
         }[self.value]
